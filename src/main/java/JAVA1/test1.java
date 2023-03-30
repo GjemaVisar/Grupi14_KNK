5d67bb929f6d7d0263121c986610fedf570782a5
Suhejl1 <121341827+Suhejl1@users.noreply.github.com>
2023-03-30 22:40:33 +0200

    first commit

--- src/main/java/JAVA1/test1.java
+++ src/main/java/JAVA1/test1.java
@@ -7,14 +7,6 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class test1 extends Application {
-    public void start (Stage stage){
-        Button btn = new Button("OK");
-        Pane pane = new Pane();
-        pane.getChildren().add(btn);
+public class test1{
 
-        Scene sc = new Scene(pane,200,200);
-        stage.setScene(sc);
-        stage.show();
-    }
 }
