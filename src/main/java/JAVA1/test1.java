bba91bc8564b8db202975cb5dfdd09be9a541a6e
Suhejl1 <121341827+Suhejl1@users.noreply.github.com>
2023-03-30 22:31:28 +0200

    first commit

--- src/main/java/JAVA1/test1.java
+++ src/main/java/JAVA1/test1.java
@@ -1,4 +1,20 @@
 package JAVA1;
 
-public class test1 {
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class test1 extends Application {
+    public void start (Stage stage){
+        Button btn = new Button("OK");
+        Pane pane = new Pane();
+        pane.getChildren().add(btn);
+
+        Scene sc = new Scene(pane,200,200);
+        stage.setScene(sc);
+        stage.show();
+    }
 }
