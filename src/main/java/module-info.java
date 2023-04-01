66fdf50285f293cc8bc8d36bdd3f0aef87eb4315
rain051803 <osmanajrigon3@gmail.com>
2023-04-01 15:29:08 +0200

    Test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,11 +5,10 @@ module com.example.ushtrime1 {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-
+    opens Home to javafx.fxml;
     opens LogIn to javafx.fxml;
     opens com.example.ushtrime1 to javafx.fxml;
     exports com.example.ushtrime1;
-
     exports LogIn to javafx.graphics;
 
 
