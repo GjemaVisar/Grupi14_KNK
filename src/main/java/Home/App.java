401ef07105157f94d8da832a5d08105c0866f5cc
rron sherifi <rronsherifi48@gmail.com>
2023-04-24 19:30:23 +0200

    add home pics

--- src/main/java/Home/App.java
+++ src/main/java/Home/App.java
@@ -1,16 +1,17 @@
-package LogIn;
+package Home;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class App extends Application {
     @Override
     public void start(Stage stage ) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LogIn.App.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),600,450);
         stage.setScene(scene);
         stage.setTitle("FiekMOTORS");
