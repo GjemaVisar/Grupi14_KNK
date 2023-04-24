69205d0ce1705054085e7a808d6dcc5924b3de54
Reis Stanovci <stanovcireis@gmail.com>
2023-04-24 20:37:54 +0200

    porti3333

--- src/main/java/Services/ConnectionUtil.java
+++ src/main/java/Services/ConnectionUtil.java
@@ -9,7 +9,7 @@ public class ConnectionUtil {
 
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
-            String url = "jdbc:mysql://localhost:3306/knk2023";
+            String url = "jdbc:mysql://localhost:3333/knk2023";
             String user = "root";
             String password = "root";
             connection = DriverManager.getConnection(url, user, password);
