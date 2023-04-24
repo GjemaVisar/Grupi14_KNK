8f151548e305d1812ddf209dcb986b41be0832a1
Reis Stanovci <stanovcireis@gmail.com>
2023-04-24 20:24:54 +0200

    connectionUtil

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ushtrime1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
