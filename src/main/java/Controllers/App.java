4fb84afcc3922786e0f4c1d3890a19ec4d716389
Reis Stanovci <stanovcireis@gmail.com>
2023-05-02 22:12:59 +0200

    carController

--- src/main/java/Controllers/App.java
+++ src/main/java/Controllers/App.java
@@ -12,11 +12,11 @@ public class App extends Application {
     public void start(Stage stage ) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TabPane-Cars.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),932,675);
+        Scene scene = new Scene(fxmlLoader.load(),840,670);
         stage.setScene(scene);
         stage.setTitle("FiekMOTORS");
         stage.show();
-        stage.setFullScreen(true);
+//        stage.setFullScreen(true);
     }
 
     public static void main(String [] args){
