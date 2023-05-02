132024c781dcfb22f5e8fe3671686142e63076c8
Suhejl1 <121341827+Suhejl1@users.noreply.github.com>
2023-05-02 13:40:45 +0200

    SedanCars-Images

---
+++ src/main/java/Controllers/RegLogController.java
@@ -0,0 +1,34 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegLogController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToLogin(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToRegister(ActionEvent e) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
+
