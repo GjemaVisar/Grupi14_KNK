8b9117d1b96bb9a58e967b59b8ef17a1d65351e4
rron sherifi <rronsherifi48@gmail.com>
2023-05-02 14:07:46 +0200

    fix controller login->register

---
+++ src/main/java/Controllers/RegLogController.java
@@ -0,0 +1,48 @@
+package Controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+
+public class RegLogController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToLogin(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+
+
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToRegister(ActionEvent e) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
+
+
+
+
+
+
+
+
+
