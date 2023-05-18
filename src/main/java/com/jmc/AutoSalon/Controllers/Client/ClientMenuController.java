27c28439e72fe595541dce151bf2c76a0ef78a18
Reis Stanovci <stanovcireis@gmail.com>
2023-05-18 16:38:44 +0200

    Help change

--- src/main/java/com/jmc/AutoSalon/Controllers/Client/ClientMenuController.java
+++ src/main/java/com/jmc/AutoSalon/Controllers/Client/ClientMenuController.java
@@ -6,7 +6,11 @@ import com.jmc.AutoSalon.Views.ClientMenuOptions;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
@@ -16,6 +20,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
