787b75612301b218425f88a85c30796493e0b89c
myhedinv <myhedin.vuciterna@student.uni-pr.edu>
2023-05-18 15:33:53 +0200

    Pagination added to the cars tab using custompanes

--- src/main/java/com/jmc/AutoSalon/Controllers/Client/tableCarsController.java
+++ src/main/java/com/jmc/AutoSalon/Controllers/Client/tableCarsController.java
@@ -6,11 +6,17 @@ import com.jmc.AutoSalon.Models.dto.carFilter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.sql.*;
@@ -18,138 +24,139 @@ import java.util.ResourceBundle;
 
 public class tableCarsController implements Initializable {
     @FXML
-    public TableView<Cars> mainTable;
-    @FXML
-    public TextField emriTxt;
-    @FXML
-    public TextField modelTxt;
-    @FXML
     public Pagination mainPage;
-    @FXML
-    public TextField typeTxt;
-    @FXML
-    public TextField priceTxt;
-    @FXML
-    public TextField colorTxt;
-    @FXML
-    public TextField speedTxt;
-    @FXML
-    public TextField yearTxt;
-    @FXML
-    public Button kerkoBtn;
-    private final int rowsPerPage = 6;
-    @FXML
-    private TableColumn<Cars, Integer> numriSerikColumn;
-
-    @FXML
-    private TableColumn<Cars, String> emriColumn;
-
-    @FXML
-    private TableColumn<Cars, String> modeliColumn;
-
-    @FXML
-    private TableColumn<Cars, String> ngjyraColumn;
-
-    @FXML
-    private TableColumn<Cars, Integer> vitiColumn;
-
-    @FXML
-    private TableColumn<Cars, Double> cmimiColumn;
-
-    @FXML
-    private TableColumn<Cars, Double> maksimumiColumn;
-
-    @FXML
-    private TableColumn<Cars, Integer> tipiColumn;
-
-    @FXML
-    private TableColumn<Cars, Date> shtuarColumn;
-
-    @FXML
-    private TableColumn<Cars, Date> perditesuarColumn;
+    private final int rowsPerPage = 2;
+    public AnchorPane mainPane;
+    public FlowPane anchorPane;
+    public Button audiBtn;
+    public Button bmwBtn;
+    public Button mercBtn;
+    public Button suvBtn;
+    public Button luxBtn;
+    public Button sdnBtn;
+    public FlowPane flowPane;
+    public ButtonBar btnBar;
     ObservableList<Cars>carsTable = FXCollections.observableArrayList();
+    String name = "audi";
+    String model = "suv";
     private Node createPage(int pageIndex) {
-        mainPage.setPageCount(carsTable.size() / rowsPerPage + 1);
+        if(carsTable.size() % rowsPerPage == 0) {
+            mainPage.setPageCount(carsTable.size() / rowsPerPage);
+        } else {
+            mainPage.setPageCount(carsTable.size() / rowsPerPage + 1);
+        }
         int fromIndex = pageIndex * rowsPerPage;
         int toIndex = Math.min(fromIndex + rowsPerPage, carsTable.size());
-        mainTable.setItems(FXCollections.observableArrayList(carsTable.subList(fromIndex, toIndex)));
-        return mainTable;
+        ObservableList<Cars>tempTable = FXCollections.observableArrayList();
+        if(toIndex < fromIndex) {
+            tempTable = FXCollections.observableArrayList(carsTable.subList(toIndex, fromIndex));
+        } else {
+            tempTable = FXCollections.observableArrayList(carsTable.subList(fromIndex, toIndex));
+        }
+
+        flowPane.getChildren().clear();
+        for (Cars car : tempTable) {
+            CustomPane newPane = new CustomPane(500, 240, car);
+            flowPane.getChildren().add(newPane);
+        }
+        return flowPane;
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        numriSerikColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, Integer>("serial"));
-        emriColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, String>("name"));
-        modeliColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, String>("model"));
-        ngjyraColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, String>("color"));
-        vitiColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, Integer>("yearMade"));
-        cmimiColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, Double>("price"));
-        maksimumiColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, Double>("maxSpeed"));
-        tipiColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, Integer>("type"));
-        shtuarColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, Date>("insertedOn"));
-        perditesuarColumn.setCellValueFactory(
-                new PropertyValueFactory<Cars, Date>("updatedOn"));
-
-
+        getCars();
+        mainPage.currentPageIndexProperty().addListener((obs, oldIndex, newIndex) -> createPage((int) newIndex));
     }
-    public void searchHandler(ActionEvent actionEvent) {
-        /*
-         public TextField emriTxt;
-    public TextField modelTxt;
-    public Pagination mainPage;
-    public TextField typeTxt;
-    public TextField priceTxt;
-    public TextField colorTxt;
-    public TextField speedTxt;
-    public TextField yearTxt;*/
-        if(emriTxt.getText() == null && modelTxt.getText() == null && typeTxt.getText() == null && priceTxt.getText() == null
-                && colorTxt.getText() == null && speedTxt.getText() == null && yearTxt.getText() == null) {
-            Model.getInstance().getViewFactory().showAlert("Error","Fill atleast 1 field.");
-        } else {
-            try {
-                if(!carsTable.isEmpty()) { carsTable.clear(); }
-                carFilter filter = new carFilter();
-                filter.setName(emriTxt.getText());
-                filter.setModel(modelTxt.getText());
-                filter.setType(typeTxt.getText());
-                if(!priceTxt.getText().isEmpty()) { filter.setPrice(Double.parseDouble(priceTxt.getText())); }
-                filter.setColor(colorTxt.getText());
-                if(!speedTxt.getText().isEmpty()) { filter.setMaxSpeed(Double.parseDouble(speedTxt.getText())); }
-                if(!yearTxt.getText().isEmpty()) { filter.setYearMade(Integer.parseInt(yearTxt.getText())); }
-                ResultSet result = filter.sqlQueryCars();
+    public void getCars() {
+        try {
+            if(!carsTable.isEmpty()) { carsTable.clear(); }
+            carFilter filter = new carFilter();
+            filter.setName(name);
+            filter.setModel(model);
+            ResultSet result = filter.sqlQueryCars();
 
-                while (result.next()) {
-                    int carId = result.getInt("numri_serik");
-                    String carName = result.getString("c_name");
-                    String carModel = result.getString("car_model");
-                    String carType = result.getString("car_type");
-                    double carPrice = result.getDouble("price_c");
-                    String carColor = result.getString("color");
-                    double carMaxSpeed = result.getDouble("max_speed");
-                    int carYear = result.getInt("year_c");
-                    int quantity = result.getInt("quantity");
-                    String carImage = result.getString("car_image");
-                    Date carInsertedOn = result.getDate("inserted_on");
-                    Date carUpdatedOn = result.getDate("updated_on");
+            while (result.next()) {
+                int carId = result.getInt("numri_serik");
+                String carName = result.getString("c_name");
+                String carModel = result.getString("car_model");
+                String carType = result.getString("car_type");
+                double carPrice = result.getDouble("price_c");
+                String carColor = result.getString("color");
+                double carMaxSpeed = result.getDouble("max_speed");
+                int carYear = result.getInt("year_c");
+                int quantity = result.getInt("quantity");
+                String carImage = result.getString("car_image");
+                Date carInsertedOn = result.getDate("inserted_on");
+                Date carUpdatedOn = result.getDate("updated_on");
 
 
-                    Cars car = new Cars(carId, carName, carModel, carType, carPrice, carColor, carMaxSpeed, carYear,quantity, carImage, carInsertedOn, carUpdatedOn);
-                    carsTable.add(car);
-                    System.out.println(car);
-                }
-                carFilter.closeConnection();
-                mainPage.setPageFactory(this::createPage);
-            } catch (SQLException e) {
-                Model.getInstance().getViewFactory().showAlert("Error",e.getMessage());
+                Cars car = new Cars(carId, carName, carModel, carType, carPrice, carColor, carMaxSpeed, carYear,quantity, carImage, carInsertedOn, carUpdatedOn);
+                carsTable.add(car);
+                System.out.println(car);
             }
+            carFilter.closeConnection();
+            mainPage.setPageFactory(pageIndex -> createPage(pageIndex));
+        } catch (SQLException e) {
+            Model.getInstance().getViewFactory().showAlert("Error",e.getMessage());
         }
+    }
+    public void setAudi(ActionEvent actionEvent) {
+        this.name = "audi";
+        getCars();
+    }
+    public void setBMW(ActionEvent actionEvent) {
+        this.name = "bmw";
+        getCars();
+    }
+    public void setMerc(ActionEvent actionEvent) {
+        this.name = "MercedesBenz";
+        getCars();
+    }
+    public void setSUV(ActionEvent actionEvent) {
+        this.model = "suv";
+        getCars();
+    }
+    public void setL(ActionEvent actionEvent) {
+        this.model = "luxury";
+        getCars();
+    }
+    public void setSdn(ActionEvent actionEvent) {
+        this.model = "sedan";
+        getCars();
+    }
+
+}
+
+class CustomPane extends Pane {
+    public CustomPane(double width, double height ,Cars car) {
+        super();
+        super.setWidth(width);
+        super.setHeight(height);
+        this.draw(car);
+    }
+    public void draw(Cars car) {
+        Image carImg = new Image((getClass().getResource("/Images/" + car.getModel() + "/" + car.getCarImage())).toString(), 300, 225, false, false);
+        ImageView imgV = new ImageView(carImg);
+        Label label = new Label(car.toString());
+        Button btn = new Button("TEST DRIVE");
+        btn.setLayoutX(800);
+        btn.setLayoutY(100);
+        btn.setStyle("-fx-background-color: \n" +
+                "        #000000,\n" +
+                "        linear-gradient(#7ebcea, #2f4b8f),\n" +
+                "        linear-gradient(#426ab7, #263e75),\n" +
+                "        linear-gradient(#395cab, #223768);\n" +
+                "    -fx-background-insets: 0,1,2,3;\n" +
+                "    -fx-background-radius: 3,2,2,2;\n" +
+                "    -fx-padding: 12 30 12 30;\n" +
+                "    -fx-text-fill: white;");
+        btn.setOnAction((new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                System.out.println("You clicked: " + car);
+            }
+        }));
+        label.setLayoutX(450);
+        label.setLayoutY(112.5);
+        super.getChildren().addAll(imgV, label, btn);
     }
 }
--- src/main/java/com/jmc/AutoSalon/Models/dto/carFilter.java
+++ src/main/java/com/jmc/AutoSalon/Models/dto/carFilter.java
@@ -6,55 +6,23 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class carFilter {
-    private String name;
+    private String name = "Audi";
 
-    private String model;
-
-    private String type;
-    private double price = -1;
-    private  String color;
-    private double maxSpeed = -1;
-    private int yearMade = -1;
+    private String model = "SUV";
     PreparedStatement preparedStatement;
     ArrayList<String>array = new ArrayList<String>();
     public static Connection conn;
     String sql = "SELECT * FROM cars WHERE 1 = 1 ";
     public void checkFilters() throws SQLException {
-        System.out.println(!this.color.isEmpty());
         if(!this.name.isEmpty()) {
             this.sql += "AND c_name LIKE ? ";
             array.add("name");
-            System.out.println("name was added");
         }
         if(!this.model.isEmpty()) {
             this.sql += "AND car_model LIKE ? ";
             array.add("model");
-            System.out.println("model was added");
-        }
-        if(!this.type.isEmpty()) {
-            this.sql += "AND car_type LIKE ? ";
-            array.add("type");
-            System.out.println("type was added");
-        }
-        if(this.price != -1 ) {
-            this.sql += "AND price_c = ? ";
-            array.add("price");
-            System.out.println("price was added");
-        }
-        if(!this.color.isEmpty()) {
-            this.sql += "AND color LIKE ? ";
-            array.add("color");
-        }
-        if(this.maxSpeed != -1 ) {
-            this.sql += "AND max_speed = ? ";
-            array.add("maxSpeed");
-        }
-        if(this.yearMade != -1 ) {
-            this.sql += "AND year_c = ? ";
-            array.add("yearMade");
         }
         conn = ConnectionUtil.getConnection();
-        System.out.println(this.sql);
         preparedStatement = conn.prepareStatement(this.sql);
         addStatements();
     }
@@ -70,21 +38,6 @@ public class carFilter {
                     case "model":
                         preparedStatement.setString(count, this.getModel());
                         break;
-                    case "type":
-                        preparedStatement.setString(count, this.getType());
-                        break;
-                    case "price":
-                        preparedStatement.setDouble(count, this.getPrice());
-                        break;
-                    case "color":
-                        preparedStatement.setString(count, this.getColor());
-                        break;
-                    case "maxSpeed":
-                        preparedStatement.setDouble(count, this.getMaxSpeed());
-                        break;
-                    case "yearMade":
-                        preparedStatement.setInt(count, this.getYearMade());
-                        break;
                 }
                 count ++;
             }
@@ -93,7 +46,7 @@ public class carFilter {
     }
     public ResultSet sqlQueryCars() throws SQLException {
         checkFilters();
-
+        System.out.println(this.sql);
         ResultSet result = preparedStatement.executeQuery();
         return result;
     }
@@ -116,45 +69,6 @@ public class carFilter {
     public void setModel(String model) {
         this.model = model;
     }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public double getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    public void setMaxSpeed(double maxSpeed) {
-        this.maxSpeed = maxSpeed;
-    }
-
-    public int getYearMade() {
-        return yearMade;
-    }
-
-    public void setYearMade(int yearMade) {
-        this.yearMade = yearMade;
-    }
+    public void resetSql() { this.sql = "SELECT * FROM cars WHERE 1 = 1 "; }
 
 }
