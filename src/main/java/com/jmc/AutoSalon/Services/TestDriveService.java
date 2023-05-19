ef37da7da9a38fb12d9e38bda27df2adae092f86
rron sherifi <rronsherifi48@gmail.com>
2023-05-19 23:01:57 +0200

    add alerts

--- src/main/java/com/jmc/AutoSalon/Services/TestDriveService.java
+++ src/main/java/com/jmc/AutoSalon/Services/TestDriveService.java
@@ -1,5 +1,6 @@
 package com.jmc.AutoSalon.Services;
 
+import com.jmc.AutoSalon.Models.Model;
 import com.jmc.AutoSalon.Repository.Interfaces.TestDriveRepositoryInterface;
 import com.jmc.AutoSalon.Repository.RepositoryTestDrive;
 import com.jmc.AutoSalon.Services.Interfaces.TestDriveServiceInterface;
@@ -20,14 +21,14 @@ public class TestDriveService implements TestDriveServiceInterface {
         LocalDate today_date = LocalDate.now();
         if(date_picked.toLocalDate().isBefore(today_date)){
             TestDriveService.PopupMessage = "You cannot choose date from the past. Please choose today or dates in the future!";
-            System.out.println(TestDriveService.PopupMessage);
+            Model.getInstance().getViewFactory().showAlert("Date picking error",TestDriveService.PopupMessage);
             return;
         }
         boolean status = this.testDriveRepo.is_date_car_busy(car,date_picked);
         if(status){
             TestDriveService.PopupMessage = "Unfortunately there already is a reservation for this car and date. Feel free" +
                     "to choose other days!";
-            System.out.println(TestDriveService.PopupMessage);
+            Model.getInstance().getViewFactory().showAlert("Reservation error!",TestDriveService.PopupMessage);
         }else{
             this.testDriveRepo.insertTestDrive(user,car,date_picked);
             TestDriveService.PopupMessage = "Reservation made successfuly. We wait for you at our offices!";
